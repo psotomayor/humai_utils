081cb66107fc3facea317c998dc3e37e9459fcf7
psotomayor <pablosotomayor.fcag@gmail.com>
2022-12-20 18:20:08 -0300

    Mi primer paquete en Python

---
+++ humai_utils/__init__.py
@@ -0,0 +1,1 @@
+from .functions import saludar_comunidad, informar_clase
---
+++ humai_utils/functions.py
@@ -0,0 +1,9 @@
+# No usamos este paquete pero igual lo importamos para generar una dependencia.
+
+import requests as re
+
+def saludar_comunidad():
+	print('Hola comunidad de Humai')
+
+def informar_clase():
+	print('La siguiente clase es el lunes')
